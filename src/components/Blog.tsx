9a432c9163b86867b11b75d8cb7afdd2b29223d0
Rayen Mabrouk <mabroukr999@gmail.com>
2025-01-16 15:32:06 +0100

    feat: update Blog component to fetch articles from DEV API and enhance feed type definition

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 import axios from "axios";
 import { parseStringPromise } from "xml2js";
-import { feedType } from "../types/feedType";
+import { FeedType } from "../types/feedType";
 
 export default async function Blog() {
-  const fetchRSSFeed = async (url: string): Promise<feedType | null> => {
+  const fetchRSSFeed = async (url: string): Promise<FeedType[] | null> => {
     try {
       const response = await axios.get(url);
-      const parsedData = await parseStringPromise(response.data, {
-        explicitArray: false,
-      });
-      return parsedData.rss.channel;
+      // const parsedData = await parseStringPromise(response.data, {
+      //   explicitArray: false,
+      // });
+      console.log("🚀 ~ fetchRSSFeed ~ parsedData:", response.data);
+      return response.data;
     } catch (error) {
       console.error("Error fetching RSS feed:", error);
       return null;
     }
   };
-  const feed = await fetchRSSFeed("https://dev.to/feed/rayenmabrouk");
+  const feed = await fetchRSSFeed(
+    "https://dev.to/api/articles?username=rayenmabrouk",
+  );
   const randomColors = [
     "text-orange-700",
     "text-blue-700",
@@ -37,37 +40,51 @@ export default async function Blog() {
           Latest Blog Articles
         </span>
         <div className="w-full">
-          {feed && feed.item.length > 0 ? (
+          {feed && feed.length > 0 ? (
             <div className="mx-auto flex w-[68rem] flex-col">
               <h2 className="text-[1.5rem] font-semibold text-color1">
-                {feed.description}
+                {
+                  "The latest articles on DEV Community by Rayen Mabrouk (@rayenmabrouk)."
+                }
               </h2>
               <div className="flex flex-col items-center gap-8 px-10 pt-16 perspective">
-                {feed.item
-                  ?.filter((_e, i) => i < 5)
-                  .sort((a, b) => +new Date(b.pubDate) - +new Date(a.pubDate))
+                {feed
+                  // ?.filter((_e, i) => i < 5)
+                  .sort(
+                    (a, b) =>
+                      b.public_reactions_count - a.public_reactions_count,
+                  )
                   .map((e, i) => (
                     <div
                       key={i}
-                      className={`flex h-full min-h-[18rem] max-w-[40rem] cursor-pointer flex-row justify-center gap-4 rounded-lg border border-gray-100 bg-white pb-8 pl-4 pr-6 pt-4 shadow-lg transition-all duration-200 ease-linear preserve-3d backface-hidden hover:z-50 hover:rotate-0 hover:scale-110 hover:shadow-xl ${i % 2 === 0 ? "self-start" : "self-end"}`}
+                      className={`flex h-full min-h-[15rem] max-w-[40rem] cursor-pointer flex-col justify-center gap-4 overflow-hidden rounded-lg border border-gray-100 bg-white p-2 pb-8 shadow-lg transition-all duration-200 ease-linear preserve-3d backface-hidden hover:z-50 hover:rotate-0 hover:scale-110 hover:shadow-xl ${i % 2 === 0 ? "self-start" : "self-end"}`}
                     >
                       <Image
-                        src={feed.image.url}
+                        src={e.social_image}
                         alt=""
-                        width={64}
-                        height={64}
-                        className="size-16 justify-self-start rounded-full"
+                        width={500}
+                        height={500}
+                        className="h-[9rem] w-full justify-self-center rounded-t-md object-cover"
                       />
-                      <div className="flex flex-col gap-3">
+                      <div className="flex flex-col gap-3 px-6">
                         <div className="flex flex-col gap-0.5">
-                          <span className="text-2xl font-semibold">
-                            {e.title}
-                          </span>
+                          <div className="mb-2 flex flex-row items-start gap-2">
+                            <Image
+                              src={e.user.profile_image}
+                              alt=""
+                              width={120}
+                              height={120}
+                              className="mt-1 size-16 justify-self-start rounded-full"
+                            />
+                            <span className="text-2xl font-semibold">
+                              {e.title}
+                            </span>
+                          </div>
                           <span className="text-color5">
-                            {new Date(e.pubDate).toLocaleString()}
+                            {new Date(e.published_at).toLocaleString()}
                           </span>
                           <span className="flex flex-row gap-2 text-sm">
-                            {e.category?.map((cat, catI) => {
+                            {e.tag_list.map((cat, catI) => {
                               return (
                                 <span
                                   className={
@@ -82,15 +99,21 @@ export default async function Blog() {
                             })}
                           </span>
                         </div>
+                        <div className="flex flex-row gap-4 text-[1.3rem]">
+                          <div className="flex flex-row gap-1">
+                            <span>💖</span>
+                            {e.public_reactions_count}
+                          </div>
+                          <div className="flex flex-row gap-1">
+                            <span>💬</span>
+                            {e.comments_count}
+                          </div>
+                        </div>
+
                         <div className="text-[0.9rem]">
-                          <span>
-                            {e.description
-                              .replace(/<[^>]*>?/gm, "")
-                              .slice(0, 250)}
-                            ...
-                          </span>
+                          <span>{e.description}</span>
                           <a
-                            href={e.link}
+                            href={e.url}
                             target="_blank"
                             rel="noreferrer"
                             className="ml-1 text-color4 hover:underline"
--- src/types/feedType.tsx
+++ src/types/feedType.tsx
@@ -1,29 +1,66 @@
-export type feedType = {
-  title: "DEV Community: Rayen Mabrouk";
-  description: "The latest articles on DEV Community by Rayen Mabrouk (@ryanmabrouk).";
-  link: "https://dev.to/ryanmabrouk";
-  image: {
-    url: "https://media.dev.to/cdn-cgi/image/width=90,height=90,fit=cover,gravity=auto,format=auto/https:%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F1309840%2F7e42f5db-fa44-448b-8f75-e6d86fe63f01.jpeg";
-    title: "DEV Community: Rayen Mabrouk";
-    link: "https://dev.to/ryanmabrouk";
+// export type feedType = {
+//   title: "DEV Community: Rayen Mabrouk";
+//   description: "The latest articles on DEV Community by Rayen Mabrouk (@ryanmabrouk).";
+//   link: "https://dev.to/ryanmabrouk";
+//   image: {
+//     url: "https://media.dev.to/cdn-cgi/image/width=90,height=90,fit=cover,gravity=auto,format=auto/https:%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F1309840%2F7e42f5db-fa44-448b-8f75-e6d86fe63f01.jpeg";
+//     title: "DEV Community: Rayen Mabrouk";
+//     link: "https://dev.to/ryanmabrouk";
+//   };
+//   "atom:link": {
+//     $: {
+//       rel: "self";
+//       type: "application/rss+xml";
+//       href: "https://dev.to/feed/ryanmabrouk";
+//     };
+//   };
+//   language: string;
+//   item: [
+//     {
+//       title: string;
+//       "dc:creator": string;
+//       pubDate: string;
+//       link: string;
+//       guid: string;
+//       description: string;
+//       category: string[];
+//     },
+//   ];
+// };
+
+export type FeedType = {
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  readable_publish_date: string;
+  slug: string;
+  path: string;
+  url: string;
+  comments_count: number;
+  public_reactions_count: number;
+  collection_id: number | null;
+  published_timestamp: string;
+  positive_reactions_count: number;
+  cover_image: string | null;
+  social_image: string;
+  canonical_url: string;
+  created_at: string;
+  edited_at: string | null;
+  crossposted_at: string | null;
+  published_at: string;
+  last_comment_at: string;
+  reading_time_minutes: number;
+  tag_list: string[];
+  tags: string;
+  user: {
+    name: string;
+    username: string;
+    twitter_username: string | null;
+    github_username: string;
+    user_id: number;
+    website_url: string | null;
+    profile_image: string;
+    profile_image_90: string;
   };
-  "atom:link": {
-    $: {
-      rel: "self";
-      type: "application/rss+xml";
-      href: "https://dev.to/feed/ryanmabrouk";
-    };
-  };
-  language: string;
-  item: [
-    {
-      title: string;
-      "dc:creator": string;
-      pubDate: string;
-      link: string;
-      guid: string;
-      description: string;
-      category: string[];
-    },
-  ];
 };
